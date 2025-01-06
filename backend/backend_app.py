92817563f5b7a83358241a4472b64f20debc6aa5
singhpooja-tech <poojasingh3018@gmail.com>
2025-01-06 22:57:38 +0100

    create list endpoint

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -12,3 +12,24 @@ POSTS = [
 
 SWAGGER_URL = "/api/docs"
 API_URL = "/static/masterblog.json"
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Masterblog API"
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
+id_stat = len(POSTS) + 1
+
+def new_id_gen():
+    """Generates a unique id using our global id counter 'id_stat' """
+    global id_stat
+    new_id = id_stat
+    id_stat += 1
+    return new_id
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5002, debug=True)
