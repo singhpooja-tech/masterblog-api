0df88959bddc7f1f0abfabfa0a86305a398c68a2
singhpooja-tech <poojasingh3018@gmail.com>
2025-01-06 22:52:09 +0100

    initialise flask and setting the CORS

---
+++ backend/backend_app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
+
+app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
+
+POSTS = [
+    {"id": 1, "title": "First post", "content": "This is the first post."},
+    {"id": 2, "title": "Second post", "content": "This is the second post."},
+]
+
+SWAGGER_URL = "/api/docs"
+API_URL = "/static/masterblog.json"
